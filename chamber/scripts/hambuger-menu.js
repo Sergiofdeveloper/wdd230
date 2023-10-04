b0d6a9791d2cad32a7d2b2999c08d241d4ce2a2e
Sergiofdeveloper <flo22013@byui.edu>
2023-10-04 20:37:35 +0200

    updates

--- chamber/scripts/hambuger-menu.js
+++
@@ -1,7 +0,0 @@
-const menuButton = document.querySelector('.menu-button');
-const menu = document.querySelector('.menu');
-
-menuButton.addEventListener('click', () => {
-    menu.classList.toggle('show-menu');
-    menuButton.classList.toggle('open'); // Add or remove the "open" class
-});
---
+++ chamber/scripts/hamburger-menu.js
@@ -0,0 +1,14 @@
+const hamburgerButton = document.getElementById('hamburger');
+const menuList = document.querySelector('nav ul');
+
+function toggleMenu() {
+    if (menuList.style.display === 'block') {
+        menuList.style.display = 'none';
+        hamburgerButton.innerHTML = '&#8801;';
+    } else {
+        menuList.style.display = 'block';
+        hamburgerButton.innerHTML = '✕';
+    }
+}
+
+hamburgerButton.addEventListener('click', toggleMenu);
