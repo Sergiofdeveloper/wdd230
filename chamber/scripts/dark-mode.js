58be039aec63b856e9e9c40c6149708441586e13
Sergiofdeveloper <flo22013@byui.edu>
2023-10-12 23:33:22 +0200

    dark mode updates

--- chamber/scripts/dark-mode.js
+++ chamber/scripts/dark-mode.js
@@ -1,5 +1,13 @@
 const menuToggle = document.getElementById('menuToggle');
 const contentWrapper = document.getElementById('content-wrapper');
+const header = document.querySelector('header');
+const container = document.querySelector('.container');
+const footer = document.querySelector('footer');
+const currentEvents = document.querySelector('.current-events');
+const weatherCard = document.querySelector('.weather-card');
+const elementsToApplyDarkMode = document.querySelectorAll(
+    '.company-advertisements, .advertisement'
+  );const cta = document.querySelector('.cta');
 let isDarkMode = false; // Initial state
 
 menuToggle.addEventListener('change', function () {
@@ -7,11 +15,37 @@ menuToggle.addEventListener('change', function () {
         // Turn on dark mode
         contentWrapper.style.backgroundColor = "#000";
         contentWrapper.style.color = "#fff";
+        header.style.backgroundColor = "#000";
+        container.style.backgroundColor = "#000";
+        footer.style.backgroundColor = "#000";
+        currentEvents.style.backgroundColor = "#000";
+        currentEvents.style.color = "#fff";
+        weatherCard.style.backgroundColor = "#000";
+        weatherCard.style.color = "#fff";
+        cta.style.backgroundColor = "#000";
+        cta.style.color = "#fff";
+        elementsToApplyDarkMode.forEach((element) => {
+            element.style.backgroundColor = "#000";
+            element.style.color = "#fff";
+          });
         isDarkMode = true;
     } else {
         // Turn off dark mode
         contentWrapper.style.backgroundColor = "#eee";
         contentWrapper.style.color = "#000";
+        header.style.backgroundColor = "#a02c32";
+        container.style.backgroundColor = "transparent";
+        footer.style.backgroundColor = "#a02c32";
+        currentEvents.style.backgroundColor = "#fbefee";
+        currentEvents.style.color = "#000";
+        weatherCard.style.backgroundColor = "#fbefee";
+        weatherCard.style.color = "#000";
+        cta.style.backgroundColor = "#fbefee";
+        cta.style.color = "#000";
+        elementsToApplyDarkMode.forEach((element) => {
+            element.style.backgroundColor = "#fbefee";
+            element.style.color = "#000";
+          });
         isDarkMode = false;
     }
 });
