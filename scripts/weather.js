3a0c922908adf96421942fc2cccc4d17e22e2abc
Sergiofdeveloper <flo22013@byui.edu>
2023-11-13 23:03:39 +0100

    upoload changes

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,7 +1,5 @@
- 
- 
   document.addEventListener('DOMContentLoaded', function () {
-    // Your OpenWeatherMap API key
+ 
     const apiKey = 'ae5b4837b97484434881ec52071a552a';
 
     // Coordinates for Zaragoza, Spain
@@ -11,10 +9,14 @@
     // URL for the OpenWeatherMap API
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
-    // Selecting elements to update
-    const temperatureElement = document.querySelector('.card-information p:nth-child(2)');
-    const descriptionElement = document.querySelector('.card-information p:nth-child(3)');
-    const weatherIconElement = document.querySelector('.card-information .weather-icon');
+
+    const temperatureElement = document.getElementById('temperature');
+    const descriptionElement = document.getElementById('description');
+    const weatherIconElement = document.querySelector('.weather-icon');
+    
+    function capitalizeWords(str) {
+        return str.replace(/\b\w/g, char => char.toUpperCase());
+      }
 
     // Fetching data from OpenWeatherMap
     fetch(apiUrl)
@@ -22,11 +24,11 @@
       .then(data => {
         // Updating the information card with weather data
         const temperature = data.main.temp;
-        const description = data.weather[0].description;
+        const description = capitalizeWords(data.weather[0].description);
         const iconCode = data.weather[0].icon;
 
         temperatureElement.textContent = `${temperature}°C`;
-        descriptionElement.textContent = description;
+        descriptionElement.textContent = `Condition: ${description}`;
 
         // Updating the weather icon
         const iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
