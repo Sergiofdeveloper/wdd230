6a6a7a733f969624cf2a7bdc22e298ac762cc754
Sergiofdeveloper <flo22013@byui.edu>
2023-11-24 17:36:45 +0100

    upload pictures

---
+++ rental/scripts/hamburguer-menu.js
@@ -0,0 +1,5 @@
+
+    function toggleMenu() {
+        var menu = document.querySelector('.menu-section ul');
+        menu.style.display = (menu.style.display === 'none' || menu.style.display === '') ? 'block' : 'none';
+    }
