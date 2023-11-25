e94a62a4183107e9f74fd4cbeb4fdeb7ead95b15
Sergiofdeveloper <flo22013@byui.edu>
2023-11-25 23:34:13 +0100

    .

--- chamber/scripts/dark-mode.js
+++ chamber/scripts/dark-mode.js
@@ -4,7 +4,6 @@ const header = document.querySelector('header');
 const container = document.querySelector('.container');
 const footer = document.querySelector('footer');
 const currentEvents = document.querySelector('.current-events');
-const events = document.querySelector('.events');
 const weatherCard = document.querySelector('.weather-card');
 const elementsToApplyDarkMode = document.querySelectorAll(
     '.company-advertisements, .advertisement'
@@ -19,9 +18,9 @@ menuToggle.addEventListener('change', function () {
         header.style.backgroundColor = "#000";
         container.style.backgroundColor = "#000";
         footer.style.backgroundColor = "#000";
-        currentEvents.style.backgroundColor = "https://sergiofdeveloper.github.io/wdd230/chamber/images/dark-mode.jpg";
+        currentEvents.style.backgroundColor = "#000";
         currentEvents.style.color = "#fff";
-        weatherCard.style.backgroundColor = "https://sergiofdeveloper.github.io/wdd230/chamber/images/dark-mode.jpg";
+        weatherCard.style.backgroundColor = "#000";
         weatherCard.style.color = "#fff";
         cta.style.backgroundColor = "#000";
         cta.style.color = "#fff";
@@ -37,7 +36,9 @@ menuToggle.addEventListener('change', function () {
         header.style.backgroundColor = "#a02c32";
         container.style.backgroundColor = "transparent";
         footer.style.backgroundColor = "#a02c32";
+        currentEvents.style.backgroundColor = "#fbefee";
         currentEvents.style.color = "#000";
+        weatherCard.style.backgroundColor = "#fbefee";
         weatherCard.style.color = "#000";
         cta.style.backgroundColor = "#fff";
         cta.style.color = "#000";
