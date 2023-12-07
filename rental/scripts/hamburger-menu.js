557053793af1dda4fb30db64149ee826f318b09c
Sergiofdeveloper <flo22013@byui.edu>
2023-12-07 23:32:26 +0100

    .

--- rental/scripts/hamburger-menu.js
+++ rental/scripts/hamburger-menu.js
@@ -6,4 +6,21 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log('Botón de menú clicado');
         menuLinks.classList.toggle('show-menu');
     });
+
+    // Agrega un evento clic a cada enlace del menú
+    var navItems = document.querySelectorAll('.menu-links a');
+    navItems.forEach(function (item) {
+        item.addEventListener('click', function () {
+            // Oculta el menú después de hacer clic en un enlace
+            menuLinks.classList.remove('show-menu');
+
+            // Remueve la clase 'active' de todos los enlaces
+            navItems.forEach(function (navItem) {
+                navItem.classList.remove('active');
+            });
+
+            // Agrega la clase 'active' solo al enlace clicado
+            item.classList.add('active');
+        });
+    });
 });
