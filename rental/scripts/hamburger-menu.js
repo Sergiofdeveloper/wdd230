6f3027c9037c796204827e4d59ba8495d838e18a
Sergiofdeveloper <flo22013@byui.edu>
2023-12-08 00:00:38 +0100

    hamburguer menu

--- rental/scripts/hamburger-menu.js
+++ rental/scripts/hamburger-menu.js
@@ -1,26 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
+    var menuLinksContainer = document.querySelector('.menu-links');
+
+    // Agrega la clase show-menu al cargar la página
+    menuLinksContainer.classList.add('show-menu');
+
     var menuBtn = document.querySelector('.menu-btn');
-    var menuLinks = document.querySelector('.menu-links');
 
     menuBtn.addEventListener('click', function () {
-        console.log('Botón de menú clicado');
-        menuLinks.classList.toggle('show-menu');
+        menuLinksContainer.classList.toggle('show-menu');
     });
 
-    // Agrega un evento clic a cada enlace del menú
-    var navItems = document.querySelectorAll('.menu-links a');
-    navItems.forEach(function (item) {
-        item.addEventListener('click', function () {
-            // Oculta el menú después de hacer clic en un enlace
-            menuLinks.classList.remove('show-menu');
+    var menuLinks = document.querySelectorAll('.menu-links a');
 
-            // Remueve la clase 'active' de todos los enlaces
-            navItems.forEach(function (navItem) {
-                navItem.classList.remove('active');
-            });
-
-            // Agrega la clase 'active' solo al enlace clicado
-            item.classList.add('active');
-        });
+    menuLinks.forEach(function (link) {
+        if (link.href === window.location.href) {
+            link.parentNode.classList.add('active');
+        }
     });
 });
