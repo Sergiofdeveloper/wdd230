f6a22303b05652d7fa50345d0421139b44afc370
Sergiofdeveloper <flo22013@byui.edu>
2023-12-12 23:46:29 +0100

    upoload

--- rental/scripts/last-modified.js
+++
@@ -1,3 +0,0 @@
-const lastModified = document.getElementById('last-modified');
-const lastModifiedDate = new Date(document.lastModified);
-lastModified.textContent = lastModifiedDate.toLocaleString();
